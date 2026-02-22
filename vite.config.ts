2cd32daf1a9c4df670112320dacd86b0d49dc2f3
undg <und3rdg@gmail.com>
2026-02-22 22:45:40 +0000

    setup global import

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './src/manifest.json' with { type: 'json' }
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   server: {
     host: 'localhost',
     port: 5173,
